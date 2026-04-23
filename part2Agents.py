cea2811245ebfd97e1dc341c1ba81e88aad5947c
kylelin23 <klin78@calpoly.edu>
2026-04-22 18:21:43 -0700

    add greedy

--- part2Agents.py
+++ part2Agents.py
@@ -16,7 +16,44 @@ from dataclasses import dataclass
 class WizardGreedy(ReasoningWizard):
     def evaluation(self, state: GameState) -> float:
         # TODO YOUR CODE HERE
-        raise NotImplementedError
+        wizard_locs = state.get_all_entity_locations(Wizard)
+
+        # Wizard is dead 
+        if not wizard_locs:
+            return float('-inf')
+
+        wizard_loc = wizard_locs[0]
+        portal_locs = state.get_all_tile_locations(Portal)
+        goblin_locs = state.get_all_entity_locations(Goblin)
+        crystal_locs = state.get_all_entity_locations(Crystal)
+
+        score = 0.0
+
+        # Reward being close to the portal
+        if portal_locs:
+            portal = portal_locs[0]
+            dist_to_portal = abs(wizard_loc.row - portal.row) + abs(wizard_loc.col - portal.col)
+            score -= dist_to_portal * 2  # weighted heavily
+
+        # Reward being close to crystals
+        if crystal_locs:
+            nearest_crystal_dist = min(
+                abs(wizard_loc.row - c.row) + abs(wizard_loc.col - c.col)
+                for c in crystal_locs
+            )
+            score -= nearest_crystal_dist
+
+        # Reward collected crystals
+        score += state.score * 10
+
+        # Penalize being close to goblins
+        for goblin_loc in goblin_locs:
+            dist_to_goblin = abs(wizard_loc.row - goblin_loc.row) + abs(wizard_loc.col - goblin_loc.col)
+            if dist_to_goblin == 0:
+                return float('-inf')  # dead
+            score += dist_to_goblin * 3  # stay far away
+
+        return score
 
 
 class WizardMiniMax(ReasoningWizard):
